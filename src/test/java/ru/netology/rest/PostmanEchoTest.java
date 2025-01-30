5b81bf034d45e7d091f6f88ce959579f1d207f46
Yuliya.S <yuliyamarketplace@yandex.ru>
2025-01-30 20:03:14 +0400

    fix the dependencies

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData(){
+      given()
+              .baseUri("https://postman-echo.com");
+              .body("Hi");
+              .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data"), org.hamcrest.Matchers.equalTo("Hello"));
+
+    }
+}
